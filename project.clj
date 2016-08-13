db14e5506b2061c81d02cf723ca375155a8e9fa3
mikera <mike.r.anderson.13@gmail.com>
2016-08-13 17:26:48 +0800

    Fix test setup to use core.matrix test suite

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  
-                 [net.mikera/core.matrix "0.42.0"]
-                 [net.mikera/core.matrix.testing "0.0.4"]
-                 [org.nd4j/nd4j-native "0.4-rc3.10"]]
+                 [net.mikera/core.matrix "0.53.0"]
+                 [org.nd4j/nd4j-native "0.5.0"]]
+  
+ :profiles {:dev {:dependencies [[net.mikera/core.matrix "0.53.0" :classifier "tests"]] 
+                   }} 
   )
